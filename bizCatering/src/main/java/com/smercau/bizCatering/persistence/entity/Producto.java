7d5bffb03eba1a908022ca2b4f9a04ba2d5fc5cc
Sebastian Mercau <smercau@e-friar.com.ar>
2024-08-19 15:40:05 -0300

    Creacion de clases base.

--- bizCatering/src/main/java/com/smercau/bizCatering/persistence/entity/Producto.java
+++ bizCatering/src/main/java/com/smercau/bizCatering/persistence/entity/Producto.java
@@ -5,17 +5,24 @@ import lombok.Setter;
 
 public class Producto {
 
+    @Getter @Setter
     private long idProducto;
 
+    @Getter @Setter
     private long codbar;
 
+    @Getter @Setter
     private String nombreProducto;
 
+    @Getter @Setter
     private String descripcionProducto;
 
+    @Getter @Setter
     private String categoriaProducto;
 
+    @Getter @Setter
     private double precioCosto;
 
+    @Getter @Setter
     private double precioVenta;
 }
--- bizCatering/src/main/java/com/smercau/bizCatering/persistence/entity/Ticket.java
+++ bizCatering/src/main/java/com/smercau/bizCatering/persistence/entity/Ticket.java
@@ -3,12 +3,16 @@ package com.smercau.bizCatering.persistence.entity;
 
 public class Ticket {
 
+    @Getter @Setter
     private long idTicket;
 
+    @Getter @Setter
     private Date fechaTicket;
 
+    @Getter @Setter
     private Empleado empleado;
 
+    @Getter @Setter
     private List<Productos> listadoProductos;
 
 }
