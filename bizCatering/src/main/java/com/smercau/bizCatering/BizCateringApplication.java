45203a0799aa821bd1d9f5d4ab61adfbb44b9b1b
Sebastian Mercau <sebastian.e.mercau@gmail.com>
2024-08-19 00:16:52 -0300

    Archivos de proyecto Spring Boot

---
+++ bizCatering/src/main/java/com/smercau/bizCatering/BizCateringApplication.java
@@ -0,0 +1,14 @@
+package com.smercau.bizCatering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizCateringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BizCateringApplication.class, args);
+		System.out.println("Hola");
+	}
+}
---
+++ bizCatering/src/test/java/com/smercau/bizCatering/BizCateringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smercau.bizCatering;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizCateringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
